d7f660a4eb2e5ee3d19845028a444830adcf08e7
sskaylord <notknt6@gmail.com>
2026-08-16 16:03:49 +0300

    Add MemoryReader class with native methods

---
+++ app/src/main/java/com/valorant/cheat/MemoryReader.java
@@ -0,0 +1,18 @@
+package com.valorant.cheat;
+
+public class MemoryReader {
+    static {
+        System.loadLibrary("native_esp");
+    }
+    
+    // Native metodlar
+    public static native long readLong(long address);
+    public static native float readFloat(long address);
+    public static native int findGameProcess(String packageName);
+    public static native void attachToProcess(int pid);
+    public static native void detachProcess();
+    public static native long findGWorld();
+    public static native long getActorArray(long gworld);
+    public static native int getActorCount(long gworld);
+    public static native Vector3[] getPlayerPositions(long actorArray, int actorCount);
+      }
