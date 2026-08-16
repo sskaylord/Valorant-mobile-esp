2723d887efd77f6877bd42ce38c28ffe523a0178
sskaylord <notknt6@gmail.com>
2026-08-16 16:03:01 +0300

    Create Vector3 class for 3D vector representation

---
+++ app/src/main/java/com/valorant/cheat/Vector3.java
@@ -0,0 +1,19 @@
+package com.valorant.cheat;
+
+public class Vector3 {
+    public float X;
+    public float Y;
+    public float Z;
+    
+    public Vector3() {
+        this.X = 0;
+        this.Y = 0;
+        this.Z = 0;
+    }
+    
+    public Vector3(float x, float y, float z) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+}
