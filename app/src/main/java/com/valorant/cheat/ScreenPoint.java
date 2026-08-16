9eb6db684dc2bcaabab3733b9ac53e205054068b
sskaylord <notknt6@gmail.com>
2026-08-17 00:23:08 +0300

    Add ScreenPoint class with x and y coordinates

---
+++ app/src/main/java/com/valorant/cheat/ScreenPoint.java
@@ -0,0 +1,11 @@
+package com.valorant.cheat;
+
+public class ScreenPoint {
+    public float x;
+    public float y;
+    
+    public ScreenPoint() {
+        this.x = 0;
+        this.y = 0;
+    }
+}
